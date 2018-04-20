6be7c99c6fba0e7e466a6b302e196e1af1ff4b3c
Sreekanth Pasunuru <spasunuru@gmail.com>
2018-04-20 09:14:50 -0400

    chages in the model

--- src/main/java/com/accionlabs/srik/Movie.java
+++ src/main/java/com/accionlabs/srik/Movie.java
@@ -1,10 +1,6 @@
 package com.accionlabs.srik;
 
 import java.io.Serializable;
-import java.util.Date;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 
 public class Movie implements Serializable{
 
